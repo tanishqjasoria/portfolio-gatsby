4c924e30d08533f4d8baf6603b953356f7272c6c
Tanishq Jasoria <jasoriatanishq@gmail.com>
2020-08-14 01:26:32 +0530

    Modify landing bio page

--- src/components/landing-bio.js
+++ src/components/landing-bio.js
@@ -2,6 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 import styled from "@emotion/styled"
+import { SOCIAL_LINKS } from "./constants"
 
 const Container = styled.div`
   text-align: center;
@@ -12,11 +13,11 @@ const OuterContainer = styled.div`
   align-items: center;
   justify-content: center;
   flex-direction: row;
-  height: 78vh;
+  height: 93vh;
 `
 
 const Description = styled.p`
-  padding: 0;
+  padding: 2vh;
   margin-bottom: 1rem;
   font-size: 1.4rem;
 `
@@ -24,6 +25,67 @@ const Description = styled.p`
 const NameHeader = styled.h1`
   font-size: 3.5rem;
   margin-bottom: 0;
+`
+const SocialLinks = styled.div`
+  margin-top: 20px;
+`
+
+const IconCont = styled.a`
+  margin: 10px;
+  text-decoration: none;
+  position: relative;
+  display: inline-block;
+  height: 30px;
+  width: 35px;
+  transition: all ease-in-out 0.2s;
+  
+  :hover::before {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+    white-space: nowrap;
+  }
+  ::before {
+    content: attr(data-title);
+    display: inline-block;
+    position: absolute;
+    bottom: -38px;
+    left: -6px;
+    margin: 0 auto;
+    font-size: 13px;
+    padding: 3px 10px;
+    background: #222;
+    color: #fff;
+    border-radius: 2px;
+    height: 28px;
+    line-height: 22px;
+    opacity: 0;
+    transition: opacity 150ms linear, transform 150ms linear, -webkit-transform 150ms linear;
+    transform: translate3d(0, -8px, 0);
+    z-index: 99;
+  }
+  ::after {
+    content: "";
+    position: absolute;
+    top: 35px;
+    left: 13px;
+    width: 0;
+    height: 0;
+    border-left: 5px solid transparent;
+    border-right: 5px solid transparent;
+    border-bottom: 6px solid #222;
+    opacity: 0;
+    transition: opacity 150ms linear, transform 150ms linear, -webkit-transform 150ms linear;
+    transform: translate3d(0, -8px, 0);
+    z-index: 100;
+  }
+  :hover::after {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+  }
+  // .social-links > .icon {
+  //   transition: all ease-in-out 0.2s;
+  // }
+  //
 `
 
 const LandingBio = () => (
@@ -43,6 +105,13 @@ const LandingBio = () => (
         <Container>
           <NameHeader>{data.site.siteMetadata.title}</NameHeader>
           <Description>{data.site.siteMetadata.subtitle}</Description>
+          <SocialLinks>
+            {SOCIAL_LINKS.map( ({url, text, Icon}) => (
+              <IconCont data-title={text} href={url} target="_blank">
+                <Icon size={32}/>
+              </IconCont>
+            ))}
+          </SocialLinks>
         </Container>
       </OuterContainer>
     )}
