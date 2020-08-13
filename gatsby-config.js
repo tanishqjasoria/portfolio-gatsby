8459a444e5529225ef699c31cfd70bf23b34d357
Tanishq Jasoria <jasoriatanishq@gmail.com>
2020-08-14 01:24:07 +0530

    Change configuration files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Julia Doe`,
-    subtitle: `software developer`,
-    description: `A minimal blog starter built with Gatsbyjs. The needed Gatsby files are included.`,
-    author: `@niklasmtj`,
+    title: `Tanishq Jasoria`,
+    subtitle: `Manners Maketh Man`,
+    description: ``,
+    author: `@tanishqjasoria`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -61,7 +61,6 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
