00d2beda7fc89e39cd2bdaa95e1899718ddf720b
Tanishq Jasoria <tanishq@newzera.com>
2020-08-15 15:25:51 +0530

    Use name from resume for title

--- src/components/layout.js
+++ src/components/layout.js
@@ -35,9 +35,9 @@ const Layout = ({ children }) => (
   <StaticQuery
     query={graphql`
       query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
+        resume {
+          basics {
+            name
           }
         }
       }
