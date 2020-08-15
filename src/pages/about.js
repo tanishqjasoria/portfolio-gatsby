6289d4785350f01718f17ffe8241a8ccc413f45b
Tanishq Jasoria <jasoriatanishq@gmail.com>
2020-08-15 16:00:40 +0530

    Add for cache

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,11 +5,20 @@ import SEO from "../components/seo"
 
 import Name from "../components/about/name.js"
 import AboutMe from "../components/about/aboutme"
+import Break from "../components/about/break"
+import Education from "../components/about/education"
+import Experience from "../components/about/experience"
+
 const AboutPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <Name />
+    <Break breakTitle="ABOUT ME" />
     <AboutMe />
+    <Break breakTitle="EDUCATION" />
+    <Education />
+    <Break breakTitle="WORK EXPERIENCE" />
+    <Experience />
   </Layout>
 )
 
